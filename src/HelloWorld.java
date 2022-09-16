69106bc86d5ee8a3db52d31961742a6ff9772cf8
EliasKpc <s51820@edu.campus02.at>
2022-09-16 19:12:57 +0200

    Initial Commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+    //main
+    //psvm
+
+    public static void main(String[] args) {
+        //sout
+        System.out.println("HelloWorld!");
+        System.out.println("Welcome to KONF 2022!");
+        System.out.println("Zeile 3");
+        System.out.println("Zeile 4");  //STRG+D --> Duplicate
+    }
+
+}
